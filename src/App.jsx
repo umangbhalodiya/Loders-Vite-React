9930eec6075076abdd592e4f95116ffc9467a6ae
Umang Bhalodiya <umangbhalodiya660@gmail.com>
2023-02-12 22:18:50 +0530

    cmnt

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Home } from "./Pages/Home";
+import { Header } from "./Components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;

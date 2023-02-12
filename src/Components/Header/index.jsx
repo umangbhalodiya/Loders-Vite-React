f484acd932e9f2730431800984ab7e270c5b90f6
Umang Bhalodiya <umangbhalodiya660@gmail.com>
2023-02-12 22:19:31 +0530

    header

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,16 @@
+import "./Header.scss";
+
+export const Header = () => {
+  return (
+    <div className="header_container">
+      <div className="header">
+        <div className="left_section">Loders</div>
+        <div className="right_section">
+          <div className="right_item">Hi </div>
+          <div className="right_item">Hi </div>
+          <div className="right_item">Hi </div>
+        </div>
+      </div>
+    </div>
+  );
+};

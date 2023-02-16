20ac98abd65fc6bc568f3e7dfaadc70255a1e8b5
Umang Bhalodiya <umangbhalodiya660@gmail.com>
2023-02-16 20:31:39 +0530

    cmnt

--- src/Components/LoaderComponents/index.jsx
+++ src/Components/LoaderComponents/index.jsx
@@ -5,33 +5,62 @@ import { Loader4 } from "./Loaders/Loader4";
 import { Loader5 } from "./Loaders/Loader5";
 import { Loader6 } from "./Loaders/Loader6";
 
-const css1 = `.loader {
-  width: 48px;
-  height: 48px;
-  border: 5px solid #FFF;
-  border-bottom-color: #FF3D00;
-  border-radius: 50%;
-  display: inline-block;
-  box-sizing: border-box;
-  animation: rotation 1s linear infinite;
-  }
-
-  @keyframes rotation {
-  0% {
-      transform: rotate(0deg);
-  }
-  100% {
-      transform: rotate(360deg);
-  }
-  } `;
-
 const loaders = [
-  { id: 1, component: <Loader1 />, css: css1 },
-  { id: 2, component: <Loader2 />, css: "" },
-  { id: 3, component: <Loader3 />, css: "" },
-  { id: 4, component: <Loader4 />, css: "" },
-  { id: 5, component: <Loader5 />, css: "" },
-  { id: 6, component: <Loader6 />, css: "" },
+  {
+    id: 1,
+    html: `<span class="loader1"></span>`,
+    component: <Loader1 />,
+    css: `
+    .loader {
+      width: 48px;
+      height: 48px;
+      border: 5px solid #FFF;
+      border-bottom-color: #FF3D00;
+      border-radius: 50%;
+      display: inline-block;
+      box-sizing: border-box;
+      animation: rotation 1s linear infinite;
+      }
+    
+      @keyframes rotation {
+      0% {
+          transform: rotate(0deg);
+      }
+      100% {
+          transform: rotate(360deg);
+      }
+    `,
+  },
+  {
+    id: 2,
+    html: `<span class="loader2"></span>`,
+    component: <Loader2 />,
+    css: "",
+  },
+  {
+    id: 3,
+    html: `<span class="loader3"></span>`,
+    component: <Loader3 />,
+    css: "",
+  },
+  {
+    id: 4,
+    html: `<span class="loader4"></span>`,
+    component: <Loader4 />,
+    css: "",
+  },
+  {
+    id: 5,
+    html: `<span class="loader5"></span>`,
+    component: <Loader5 />,
+    css: "",
+  },
+  {
+    id: 6,
+    html: `<span class="loader6"></span>`,
+    component: <Loader6 />,
+    css: "",
+  },
 ];
 
 export default loaders;
